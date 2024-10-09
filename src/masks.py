4ef18351a6f472880b1074f8a9f9cac987cc3cd5
Larisa Vikulova <79026284410@ya.ru>
2024-10-09 20:35:32 +0500

    First Bank-project

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+card_number_str = int()
+account_str = int()
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску: 7000 79** **** 6361"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску: **4305"""
+    return f"**{account[-4:]}"
