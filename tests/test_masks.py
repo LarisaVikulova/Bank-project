1d4f31f8690d14e8264ceb7354c623607358d58f
Larisa Vikulova <79026284410@ya.ru>
2024-10-16 23:52:47 +0500

    Add tests in project

---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+from src.masks import get_mask_card_number, get_mask_account
+import pytest
+
+
+@pytest.mark.parametrize(
+    "number, masks",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("4571354875129009", "4571 35** **** 9009"),
+        ("2504706942057145", "2504 70** **** 7145"),
+    ],
+)
+@pytest.fixture
+def test_get_mask_card_number() -> str:
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+    assert get_mask_card_number("7000792289606361456783456") == "7000 79** **** 3456"
+
+
+def test_get_mask_account() -> str:
+    assert get_mask_account("73654108430135874305") == "**4305"
+    assert get_mask_account("73654108430135874305123456") == "**3456"
